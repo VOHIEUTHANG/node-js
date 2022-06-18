f0ddeb23b5a67377594b7f960b3ee39d5fe9df86
VOHIEUTHANG <hieuthang369@gmail.com>
2022-06-18 12:23:30 +0700

    CRUP completed

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -6,7 +6,7 @@ const getUserPage = async (req, res) => {
   ]);
   res.send(rows);
 };
-const postUser = async (req, res) => {
+const createUser = async (req, res) => {
   const { firstName, lastName, email, address } = req.body;
   const result = await pool.execute(
     "INSERT INTO users (firstName,lastName,email,address) VALUES (?, ?, ?, ?)",
@@ -14,4 +14,24 @@ const postUser = async (req, res) => {
   );
   if (result[0].affectedRows > 0) res.redirect("/");
 };
-export { getUserPage, postUser };
+const deleteUser = async (req, res) => {
+  const userId = req.body.id;
+  const result = await pool.execute("DELETE FROM users WHERE id=?", [userId]);
+  if (result[0].affectedRows > 0) res.redirect("/");
+};
+const getUpdatePage = async (req, res) => {
+  const userId = req.params.userId;
+  const [user] = await pool.execute("select * from users where id = ?", [
+    userId,
+  ]);
+  res.render("update.ejs", { userInfo: user[0] });
+};
+const updateUser = async (req, res) => {
+  const { id, firstName, lastName, email, address } = req.body;
+  const result = await pool.execute(
+    "UPDATE users SET firstName = ?, lastName = ?, email = ?, address = ? where id = ? ",
+    [firstName, lastName, email, address, id]
+  );
+  if (result[0].affectedRows > 0) res.redirect("../");
+};
+export { getUserPage, createUser, deleteUser, getUpdatePage, updateUser };
--- src/route/web.js
+++ src/route/web.js
@@ -1,12 +1,21 @@
 import express from "express";
 const router = express.Router();
 import { getHomePage } from "../controller/homeController";
-import { getUserPage, postUser } from "../controller/userController";
+import {
+  getUserPage,
+  createUser,
+  deleteUser,
+  getUpdatePage,
+  updateUser,
+} from "../controller/userController";
 
 const initWebRounte = (app) => {
   router.get("/", getHomePage);
   router.get("/user/detail/:userId", getUserPage);
-  router.post("/user/post", postUser);
+  router.post("/user/post", createUser);
+  router.post("/user/delete", deleteUser);
+  router.get("/user/update/:userId", getUpdatePage);
+  router.post("/user/update", updateUser);
   return app.use("/", router);
 };
 
